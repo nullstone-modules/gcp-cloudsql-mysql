cd987a5919db4b0d5dd5098382ebc76192b12fa4
Brad Sickles <brad.sickles@gmail.com>
2023-06-07 20:17:10 -0400

    Fix naming of global address

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.0 (Unreleased)
+* Fixed creation of two mysql instances in one project. (Fixed unique global IP address)
+* Added `mysql-db-admin` to enable administration of databases/users through access module.
--- service_network.tf
+++ service_network.tf
@@ -1,5 +1,5 @@
-resource "google_compute_global_address" "private_ip_alloc" {
-  name          = "private-ip-alloc"
+resource "google_compute_global_address" "private_ip" {
+  name          = "${local.resource_name}-private-ip"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
@@ -9,7 +9,7 @@ resource "google_compute_global_address" "private_ip_alloc" {
 resource "google_service_networking_connection" "this" {
   network                 = local.vpc_id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+  reserved_peering_ranges = [google_compute_global_address.private_ip.name]
 
   depends_on = [google_project_service.service_networking]
 }
