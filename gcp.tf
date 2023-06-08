fd27da21d0b5b80ccd99bef353ae95da9f5ff5f4
Brad Sickles <brad.sickles@gmail.com>
2023-06-07 21:13:55 -0400

    Enable CloudRun APIs

--- gcp.tf
+++ gcp.tf
@@ -18,6 +18,13 @@ resource "google_project_service" "sqladmin" {
   disable_on_destroy         = false
 }
 
+// Cloud Run is needed to create a Cloud Function (db-admin)
+resource "google_project_service" "run" {
+  service                    = "run.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
 locals {
   project_id      = data.google_compute_zones.available.project
   available_zones = data.google_compute_zones.available.names
