70e4a2f66d5f7cc278a60fdfaa4b0cc6b5a92de1
Brad Sickles <brad.sickles@gmail.com>
2024-11-27 10:42:07 -0500

    Move networking setup to network module.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.1 (Nov 27, 2024)
+* Move networking setup to network module.
+* Upgrade Terraform modules.
+
 # 0.1.0 (Jun 12, 2023)
 * Fixed creation of two mysql instances in one project. (Fixed unique global IP address)
 * Added `mysql-db-admin` to enable administration of databases/users through access module.
--- db.tf
+++ db.tf
@@ -1,10 +1,10 @@
 resource "google_sql_database_instance" "this" {
   name                = local.resource_name
   database_version    = "MYSQL_${var.mysql_version}_0"
-  region              = data.google_compute_zones.available.region
+  region              = local.region
   deletion_protection = false
 
-  depends_on = [google_service_networking_connection.private_vpc_connection, google_project_service.sqladmin]
+  depends_on = [google_project_service.sqladmin]
 
   settings {
     tier              = var.instance_class
